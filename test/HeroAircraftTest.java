60d610583b46e07d29affd37ed0df3b5b5c8fe4b
BrokenSegment <2123609136@qq.com>
2025-10-06 15:14:24 +0800

    :white_check_mark: test heroAircraft

---
+++ test/HeroAircraftTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroAircraftTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void forward() {
+    }
+
+    @Test
+    void shoot() {
+    }
+
+    @Test
+    void addHp() {
+    }
+
+    @Test
+    void addshootNum() {
+    }
+
+    @Test
+    void shootCircle() {
+    }
+}
